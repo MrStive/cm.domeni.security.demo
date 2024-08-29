86c63c205cff55cee7d171ed44d7c7838972f535
domeni.ngameni <domeni.ngameni@lao-sarl.cm>
2024-08-29 17:44:45 +0100

    feat(demosecur-2): implement end point to generate token

--- demo.security/src/main/java/cm/domeni/demo/security/services/AccountServiceImpl.java
+++ demo.security/src/main/java/cm/domeni/demo/security/services/AccountServiceImpl.java
@@ -4,24 +4,35 @@ import cm.domeni.demo.security.entities.AppRole;
 import cm.domeni.demo.security.repositories.AppRoleSpringRepository;
 import cm.domeni.demo.security.repositories.AppUserSpringRepository;
 import cm.domeni.demo.security.entities.AppUser;
+import cm.domeni.demo.security.services.mapstruct.UserMapper;
+import cm.domeni.security.demo.model.CreateUserDTO;
+import cm.domeni.security.demo.model.RoleDTO;
+import cm.domeni.security.demo.model.UserDTO;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Transactional
 public class AccountServiceImpl implements AccountService {
     private final AppRoleSpringRepository appRoleSpringRepository;
     private final AppUserSpringRepository appUserSpringRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public AccountServiceImpl(AppRoleSpringRepository appRoleSpringRepository, AppUserSpringRepository appUserSpringRepository) {
+    public AccountServiceImpl(AppRoleSpringRepository appRoleSpringRepository, AppUserSpringRepository appUserSpringRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
         this.appRoleSpringRepository = appRoleSpringRepository;
         this.appUserSpringRepository = appUserSpringRepository;
+        this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public AppUser addNewUser(AppUser appUser) {
+        String pw = appUser.getPassword();
+        appUser.setPassword(passwordEncoder.encode(pw));
         return appUserSpringRepository.save(appUser);
     }
 
@@ -31,19 +42,29 @@ public class AccountServiceImpl implements AccountService {
     }
 
     @Override
-    public void addRoleToUser(String username, String roleName) {
-        appUserSpringRepository.findByUserName(username)
+    public void addRoleToUser(String userId, String roleName) {
+        appUserSpringRepository.findById(userId)
                 .ifPresent(appUser -> appUser.assignRole(roleName));
     }
 
     @Override
-    public AppUser loadUserByUsername(String username) {
-        return appUserSpringRepository.findByUserName(username).orElseThrow();
+    public List<UserDTO> listUsers() {
+        return appUserSpringRepository.findAll()
+                .stream()
+                .map(userMapper::mapToUserDTO)
+                .toList();
     }
 
     @Override
-    public List<AppUser> listUsers() {
-        System.out.printf("**********  AccountServiceImpl ****************");
-        return appUserSpringRepository.findAll();
+    public UUID createUser(CreateUserDTO userDTO) {
+        AppUser save = appUserSpringRepository.save(
+                AppUser.builder()
+                        .userName(userDTO.getUserName())
+                        .password(userDTO.getPassword())
+                        .build()
+        );
+        List<AppRole> appRoleStream = userDTO.getRoles().stream().map(RoleDTO::getName).map(s -> AppRole.builder().build()).toList();
+        appRoleSpringRepository.saveAll(appRoleStream);
+        return UUID.fromString(save.getId());
     }
 }
